ca617d9922737ceaa77e43428ff4312161a163a8
letitride <ichikawa.fumiya@gmail.com>
2020-03-24 22:45:23 +0900

    6-1 axiosライブラリによるAjax通信について

---
+++ chapter6/6-1/listbooks1.js
@@ -0,0 +1,41 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    items: [],
+    reverse: false,
+    orderIcon: "▲"
+  },
+  computed: {
+    sortedItems: function(){
+      if(this.reverse){
+        return this.items.slice().sort(function(a, b){
+          return b.price - a.price;
+        });
+      }else{
+        return this.items.slice().sort(function(a, b){
+          return a.price - b.price;
+        });
+      }
+    }
+  },
+  methods: {
+    reverseOrder: function(){
+      this.reverse = !this.reverse;
+      if(this.reverse){
+        this.orderIcon = "▼";
+      }else{
+        this.orderIcon = "▲";
+      }
+    }
+  },
+  created: function(){
+    self = this;
+    axios
+      .get("./items.json")
+      .then( function(res){
+        self.items = res.data.items;
+      }).catch( function(err){
+        console.log(err);
+      });
+  }
+});
