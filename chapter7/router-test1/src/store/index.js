de01458fc86abbc53e1104a67f4f9c5795f8c0f2
letitride <ichikawa.fumiya@gmail.com>
2020-03-26 17:37:27 +0900

    7-3 Vue RouterとVuexを使ってみよう

---
+++ chapter7/router-test1/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    message: "こんにちは"
+  },
+  mutations: {
+    changeMsg: function(state){
+      state.message = "さようなら"
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ chapter7/router-test1/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>{{storedMsg}}</h1>
+    <button @click="changeMsg">メッセージを変更</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  computed: {
+    storedMsg: function(){
+      return this.$store.state.message;
+    }
+  },
+  methods: {
+    changeMsg: function(){
+      this.$store.commit("changeMsg");
+    }
+  }
+}
+</script>
